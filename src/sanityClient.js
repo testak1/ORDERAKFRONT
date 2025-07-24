0b7ebb28672f83e5df0f142e56c51026a9d98966
testak1 <60112704+testak1@users.noreply.github.com>
2025-07-24 11:21:20 +0200

    Update sanityClient.js

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -4,7 +4,7 @@ import { createClient } from "@sanity/client";
 export const client = createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: process.env.REACT_APP_SANITY_DATASET,
-  apiVersion: "2025-07-20",
+  apiVersion: "2025-07-24",
   useCdn: false,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
